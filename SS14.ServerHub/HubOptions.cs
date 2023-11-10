8c94dfa465e341b9c3bf071c0f460150caec2aff
Skye <22365940+Skyedra@users.noreply.github.com>
2023-11-10 02:41:59 -0800

    Increase server expiry to 15min to prevent erroneous server drops

--- SS14.ServerHub/HubOptions.cs
+++ SS14.ServerHub/HubOptions.cs
@@ -4,7 +4,15 @@ public sealed class HubOptions
 {
     public const string Position = "Hub";
         
-    public float AdvertisementExpireMinutes { get; set; } = 3;
+    /// <summary>
+    /// How long until drop a server off hub list.  
+    /// 
+    /// Default for server is to ping every 2 minutes, and for hub to drop for 3, which is tight but 
+    /// theoretically okay.  However, multiple servers seem to be missing the two minute window 
+    /// consistently, and being up to ~72sec late.  Because of that, just going to set this to 15 minutes.
+    /// </summary>
+    /// <value></value>
+    public float AdvertisementExpireMinutes { get; set; } = 15; 
         
     /// <summary>
     /// When a server advertises itself with the hub, we check whether we can reach the address.
